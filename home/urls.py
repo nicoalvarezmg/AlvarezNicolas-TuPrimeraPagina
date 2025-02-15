c93797dbef8d83ccbd390f27ed1f1028ccefdf18
Nicolás Alvarez <nicoalvarezmaguna@gmail.com>
2025-02-15 20:51:16 -0300

    agrego carpeta fotos

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,8 @@
 from home.views import home
 from django.urls import path
+
+
 urlpatterns = [
-    path('pipes-home/', home, name='pipes_home')
+    path('pipes-home/', home, name='pipes_home'),
 ]
+
